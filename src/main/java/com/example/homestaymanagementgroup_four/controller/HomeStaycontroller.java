302505903d75cb19ee595bd156998a71fd248d73
LeDuyKhanh1989 <designer.expert228@gmail.com>
2023-09-25 14:03:46 +0700

    add

---
+++ src/main/java/com/example/homestaymanagementgroup_four/controller/HomeStaycontroller.java
@@ -0,0 +1,50 @@
+package com.example.homestaymanagementgroup_four.controller;
+
+import com.example.homestaymanagementgroup_four.model.Homestay;
+import com.example.homestaymanagementgroup_four.service.implement.HomeStayService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/homestay")
+public class HomeStaycontroller {
+    @Autowired
+    private HomeStayService homeStayService;
+
+    @GetMapping ("")
+    public ResponseEntity<Iterable<Homestay>> showList(){
+        Iterable<Homestay> homestays = homeStayService.findAll();
+        return new ResponseEntity<>(homestays, HttpStatus.OK);
+    }
+
+    @PostMapping("create")
+    public ResponseEntity<Homestay> create(@RequestBody Homestay homestay){
+        return new ResponseEntity<>(homeStayService.save(homestay),HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Homestay> create(@RequestBody Homestay homestay, @PathVariable ("id") Long id){
+       Optional<Homestay>  homestay1 = homeStayService.findById(id);
+       if(!homestay1.isPresent()){
+           return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+       } else {
+           homestay.setId(homestay1.get().getId());
+           homeStayService.save(homestay);
+       }
+       return new ResponseEntity<>(homestay,HttpStatus.OK);
+    }
+
+    @DeleteMapping("{id}")
+    public  ResponseEntity<Homestay> delete(@PathVariable Long id){
+        Optional<Homestay> homestay = homeStayService.findById(id);
+        if(!homestay.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } homeStayService.remove(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
